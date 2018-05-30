70200660f699d6396e84fc75f463d9af3b7cbc33
unknown <alexis.frlp@gmail.com>
2018-05-30 19:36:23 -0300

    Test de func GET tira error - corregir

--- test/test_unit.py
+++ test/test_unit.py
@@ -26,11 +26,6 @@ class OrderingTestCase(TestCase):
         db.drop_all()
         db.create_all()
 
-    # Destruimos la base de datos de test
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
     def test_iniciar_sin_productos(self):
         resp = self.client.get('/product')
         data = json.loads(resp.data)
@@ -52,30 +47,69 @@ class OrderingTestCase(TestCase):
         # Verifica que en la lista de productos haya un solo producto
         self.assertEqual(len(p), 1, "No hay productos")
 
-# Punto 1) a) No se pueda crear instancia de OrderProduct si quantity negativo
 
+    # Punto 1) a) No se pueda crear instancia de OrderProduct si quantity negativo
     def test_instance_of_OrderProduct_with_negative_quantity(self):
+        # Inicio la Orden y cargo en la BD
         o = Order(id=1)
         db.session.add (o)
+
+        # Inicio el Producto y cargo en la BD
         p = Product(id=1, name='Armario', price=800)
         db.session.add(p)
+
+        #Inicio la Order del Producto y cargo en la BD
         orderProduct = OrderProduct(order_id=1, product_id=1, quantity=-1, product=p)
         db.session.add(orderProduct)
+
         db.session.commit()
+
+        #Consulto todas las Ordenes de Producto
         ordp = OrderProduct.query.all()
+
+        #Corroboro que se halla cargado la Orden con Cantidad Negativa
         self.assertEqual(len(ordp),1, "Se creo el prod con cantidad negativa")
 
+
+    # Punto 1) b) Funcionamiento del GET
     def test_GET_function(self):
+        # Inicio la Orden y cargo en la BD
         o = Order(id=1)
         db.session.add (o)
+
+        # Inicio el Producto y cargo en la BD
         p = Product(id=1, name='Silla', price=200)
         db.session.add(p)
+
+        #Inicio la Order del Producto y cargo en la BD
         orderProduct = OrderProduct(order_id=1, product_id=1, quantity=8, product=p)
         db.session.add(orderProduct)
-        db.session.commit()
-        respGET=self.client.get('/order/1/product/1')
-        self.assert200(respGET, "No existe la orden o el producto")
 
+        db.session.commit()
+
+        #Consulto la Orden
+        respGET=self.client.get('/order/1')
+        
+        #Corroboro que se halla cargado la Orden
+        self.assert200(respGET, "No existe la orden")
+
+        #Consulto el producto
+        product = json.loads(respGET.data)
+
+        #Corroboro que se halla cargado el Producto
+        self.assertEqual(len(product),1, "No se cargo el producto")
+
+        #Consulto la Orden y  el Producto
+        respGET=self.client.get('/order/1/product/1')
+        
+        #Corroboro que se halla cargado la Orden
+        self.assert200(respGET, "No existe la orden con el producto")
+
+
+    # Destruimos la base de datos de test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 if __name__ == '__main__':
     unittest.main()
