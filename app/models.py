82665f0f13199235b2399ac295904d9a93f2bedb
unknown <alexis.frlp@gmail.com>
2018-06-14 22:36:18 -0300

    bugfix .\app\models.py:16:5: E301

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), index=True)
     price = db.Column(db.Float, index=True)
+    
     def __repr__(self):
     return '{}'.format(self.name)
 
